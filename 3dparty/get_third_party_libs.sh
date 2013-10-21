97155251bd4436a061625ab22e4031a8438962d7
Tu-Hoa Pham <pham.main@gmail.com>
2013-10-21 14:18:56 +0200

    Playing around...

--- 3dparty/get_third_party_libs.sh
+++ 3dparty/get_third_party_libs.sh
@@ -70,9 +70,10 @@ else
      sudo pacman -S libxmu libxi freeglut libusb
      #git clone git://github.com/OpenNI/OpenNI.git 
      #cd OpenNI/Platform/Linux/CreateRedist
-     #sed -e s/"python "/"python2 "/g RedistMaker > RedistMaker.tmp && mv RedistMaker.tmp RedistMaker
-     #chmod +x RedistMaker
      #./RedistMaker
+     cd ../Redist
+     #sudo ./install.sh 
+     cd ../../../../
 
      yaourt -G openni-git
      cd openni-git
@@ -81,11 +82,6 @@ else
      cd ..
      mv openni-git/src/openni-build OpenNI
      rm -rf openni-git
-
-     cd ../Redist
-     #sudo ./install.sh 
- 
-     cd ../../../../
 
 
      #should be at 3dparty dir
